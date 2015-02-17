accae039b6d2a7ac33701460cadc9ff5aade6d0d
Ian Horn <horn.imh@gmail.com>
2015-02-17 13:53:54 -0800

    Documented GPS time conversions

--- swiftnav/gpstime.pyx
+++ swiftnav/gpstime.pyx
@@ -33,21 +33,64 @@ cdef class GpsTime:
     def __set__(self, tow):
       self.gps_time.tow = tow
 
-#TODO make sure the time conversions are correct
 def gpst_components2datetime(wn, tow):
-  # TODO lookup the sign on the 16 seconds
+  """
+  Convert GPS time in terms of week number into GPS time in terms of the
+  calendar. IMPORTANTLY, the calendar GPS time is off from UTC by a number
+  of leap seconds (currently 17). The UTC to GPS time conversion can change up to twice
+  per year.
+
+  Parameters
+  ----------
+  wn : int
+    The GPS week number.
+  tow : int
+    The GPS time of week.
+
+  Returns
+  -------
+  datetime
+    The GPS time in calendar form.
+  """
   dt = datetime.timedelta(weeks=wn, seconds=tow)
-  return dt + datetime.datetime(1980, 1, 6, 0, 0, 0) + \
-      datetime.timedelta(seconds=16)
+  return dt + datetime.datetime(1980, 1, 6, 0, 0, 0)
 
 def gpst2datetime(GpsTime gpst):
+"""
+Convert a GpsTime into datetime. Both are assumed to be GPS times.
+(See note in gpst_components2datetime for explanation).
+
+Parameters
+----------
+gpst : GpsTime
+  GPS time struct storing week number and time of week.
+
+Returns
+-------
+datetime
+  GPS time stored as a datetime.
+"""
   return gpst_components2datetime(gpst.wn, gpst.tow)
 
 def datetime2gpst(timestamp):
-  # TODO lookup the sign on the 16 seconds
-  dt = timestamp - datetime.datetime(1980, 1, 6, 0, 0, 0) - \
-      datetime.timedelta(seconds=16)
+  """
+  Convert a datetime (still GPS time) into a GpsTime.
+  The datetime should be GPS time, which is (currently) 17 seconds
+  off from UTC. The UTC to GPS time conversion can change up to twice
+  per year.
+
+  Parameters
+  ----------
+  timestamp : datetime
+    GPS time in terms of a datetime object.
+
+  Returns
+  -------
+  GpsTime
+    GPS time in terms of week number and time of week.
+  """
+  dt = timestamp - datetime.datetime(1980, 1, 6, 0, 0, 0)
   wn = dt.days / 7
   tow = (dt - datetime.timedelta(weeks=wn)).total_seconds()
   #wn % 1024 would be standard, but we want a bijective tranformation.
-  return GpsTime(wn, tow) 
+  return GpsTime(wn, tow)
